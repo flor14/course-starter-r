5034367065a1c7cc0bf16c013570a49f0ae8d1f3
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-07-27 08:58:29 -0300

    Update exc_01_03.R

--- exercises/exc_01_03.R
+++ exercises/exc_01_03.R
@@ -1,17 +1,11 @@
-library(ggplot2)
+library(tidyverse)
+cars2018 <- read_csv("/datasets/cars2018.csv")
 
-mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
-  	labels=c("3gears","4gears","5gears"))
-mtcars$am <- factor(mtcars$am,levels=c(0,1),
-  	labels=c("Automatic","Manual"))
-mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
-   labels=c("4cyl","6cyl","8cyl"))
+# Print the cars2018 object
+___
 
-# Print the gear variable of mtcars
-print(____)
-
-# Assign the length of mtcars to some_var
-some_var <- ____
-
-# Uncomment this to see the plot
-# print(qplot(mpg, data=mtcars, geom="density", fill=gear, alpha=I(.5)))
+# Plot the histogram
+ggplot(cars2018, aes(x = ___)) +
+    geom_histogram(bins = 25) +
+    labs(___ = "Fuel efficiency (mpg)",
+         ___ = "Number of cars")
