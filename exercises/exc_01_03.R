613e86475fe209fdbf9b3151b3f2524cecaeef80
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-07-28 18:57:30 -0300

    Update exc_01_03.R

--- exercises/exc_01_03.R
+++ exercises/exc_01_03.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 cars2018 <- read_csv("cars2018.csv")
-
+stack <- read_csv("data/stackoverflow.csv")
 # Print the cars2018 object
 ___
-
+___
 # Plot the histogram
 ggplot(cars2018, aes(x = ___)) +
     geom_histogram(bins = 25) +
