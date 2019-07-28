a8b069e3c42afb55f510bb503eec3b099840f28b
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-07-28 18:57:58 -0300

    Update solution_01_03.R

--- exercises/solution_01_03.R
+++ exercises/solution_01_03.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 cars2018 <- read_csv("cars2018.csv")
-
+stack <- read_csv("data/stackoverflow.csv")
 # Print the cars2018 object
 cars2018
-
+stack
 # Plot the histogram
 ggplot(cars2018, aes(x = MPG)) +
     geom_histogram(bins = 25) +
